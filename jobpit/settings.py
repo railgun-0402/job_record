a78fa700147dded9c749a470ed0a5315bc417b29
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2023-08-11 20:53:04 +0900

    settings

--- jobpit/settings.py
+++ jobpit/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'jobpit.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jobpit-db',
+        'USER': 'root',
+        'PASSWORD': 'cls21214',
+        'HOST': 'localhost',
+        'PORT': '3306'
     }
 }
 
@@ -103,13 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 
 def main():
